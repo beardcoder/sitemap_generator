3261d3a245b91cb93ac26ba9d03c81f865736823
Nicole Cordes <cordes@cps-it.de>
2015-12-22 22:11:35 +0100

    [FEATURE] Add functional test for records sitemap

---
+++ Tests/Functional/Fixtures/Extensions/news_fixture/ext_tables.sql
@@ -0,0 +1,16 @@
+#
+# Table structure for table 'news'
+#
+CREATE TABLE news (
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+	deleted tinyint(4) DEFAULT '0' NOT NULL,
+	hidden tinyint(4) DEFAULT '0' NOT NULL,
+	tstamp int(11) DEFAULT '0' NOT NULL,
+	title tinytext,
+
+	sitemap_priority int(3) DEFAULT '5' NOT NULL,
+	sitemap_changefreq varchar(255) DEFAULT '' NOT NULL,
+
+	PRIMARY KEY (uid)
+);
