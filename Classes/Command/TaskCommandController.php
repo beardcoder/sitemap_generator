e132933424f007ae05bb1c08c9c06fdf8482c23c
Delaramas <markussom@posteo.de>
2016-02-02 09:11:45 +0100

    [FEATURE] Google notification service and task
    Resolves: #11

---
+++ Classes/Command/TaskCommandController.php
@@ -0,0 +1,40 @@
+<?php
+namespace Markussom\SitemapGenerator\Command;
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+use Markussom\SitemapGenerator\Service\GoogleSitemapService;
+use TYPO3\CMS\Extbase\Mvc\Controller\CommandController;
+
+/**
+ * Class TaskCommandController
+ */
+class TaskCommandController extends CommandController
+{
+    /**
+     * Send a request to google tools api for the sitemap crawling.
+     *
+     * @param string $xmlSiteUrl http://www.example.com/sitemap.xml
+     */
+    public function googleSitemapToolCommand($xmlSiteUrl)
+    {
+        $googleSitemapPing = new GoogleSitemapService($xmlSiteUrl);
+        $httpCode = $googleSitemapPing->sendRequest();
+
+        if ($httpCode === 200) {
+            $this->outputLine('success');
+        }
+
+        $this->outputLine('error');
+    }
+}
---
+++ Tests/Functional/Service/GoogleSitemapServiceTest.php
@@ -0,0 +1,42 @@
+<?php
+namespace Markussom\SitemapGenerator\Service;
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+use TYPO3\CMS\Core\Tests\FunctionalTestCase;
+
+/**
+ * Class GoogleSitemapServiceTest
+ */
+class GoogleSitemapServiceTest extends FunctionalTestCase
+{
+
+    /**
+     * @var GoogleSitemapService
+     */
+    protected $googleSitemapService;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->googleSitemapService = new GoogleSitemapService('http://www.localhost.dev/sitemap.xml');
+    }
+
+    /**
+     * @test
+     */
+    public function requestTest()
+    {
+        $this->assertEquals($this->googleSitemapService->sendRequest(), 200);
+    }
+}
