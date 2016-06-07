2d93c1a7dd4da3c11660fe8adfa98f3c5649a26c
Delaramas <markussom@posteo.de>
2016-06-07 19:46:26 +0200

    [TASK] Remove use

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,13 +1,10 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-use Markussom\SitemapGenerator\Command\TaskCommandController;
-use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
-
-ExtensionUtility::configurePlugin(
+TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
     'Markussom.sitemap_generator',
     'Pi1',
     ['Sitemap' => 'list, googleNewsList']
 );
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = TaskCommandController::class;
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = Markussom\SitemapGenerator\Command\TaskCommandController::class;
--- ext_tables.php
+++ ext_tables.php
@@ -1,9 +1,7 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-
-ExtensionManagementUtility::addStaticFile(
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     $_EXTKEY,
     'Configuration/TypoScript',
     'Sitemap Generator'
