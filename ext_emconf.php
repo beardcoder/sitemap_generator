b6580f27b1f85ae14fdf86063f5cb4ce6a123280
Delaramas <markussom@posteo.de>
2016-09-01 21:08:17 +0200

    [TASK] Set TYPO3 CMS version from 7 to 8

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,9 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.6.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '6.2.7-7.99.99',
-            'extbase' => '6.2.0-7.99.99',
-            'fluid' => '6.2.0-7.99.99',
+            'typo3' => '6.2.7-8.99.99',
             'php' => '5.5.0-0.0.0',
         ],
         'conflicts' => [],
