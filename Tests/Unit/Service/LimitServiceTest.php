2829ab38949f8a1fa1ef18b23a26ca13f2375dfe
Markus Sommer <markus.sommer@in2code.de>
2017-02-15 21:49:35 +0100

    [TASK] Add Test for limit service

---
+++ Tests/Unit/Service/LimitServiceTest.php
@@ -0,0 +1,31 @@
+<?php
+namespace Markussom\SitemapGenerator\Tests\Unit\Service;
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+class LimitServiceTest extends \TYPO3\CMS\Core\Tests\UnitTestCase
+{
+    /**
+     * @test
+     */
+    public function canReturnsTheLimitStatementForDatabaseConnection()
+    {
+        $actual = \Markussom\SitemapGenerator\Service\LimitService::getLimitString('100');
+        $this->assertTrue(is_int($actual));
+        $this->assertSame(100, $actual);
+
+        $actual = \Markussom\SitemapGenerator\Service\LimitService::getLimitString('100, 200');
+        $this->assertSame(100 . ',' . 200, $actual);
+    }
+}
