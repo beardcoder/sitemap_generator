5549813d3fafe224b58c0c8e331f4a6525051049
Markus Sommer <markussom@posteo.de>
2017-06-07 17:58:03 +0200

    [TASK] Update NamelessCoder/typo3-repository-client

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,19 +1,24 @@
 <?php
-
-$EM_CONF[$_EXTKEY] = [
-    'title' => 'Sitemap Generator',
-    'description' => 'Easy to use sitemap generator for pages and records.',
-    'category' => 'plugin',
-    'author' => 'Markus Sommer',
-    'author_email' => 'markussom@posteo.de',
-    'state' => 'stable',
-    'version' => '1.1.0',
-    'constraints' => [
-        'depends' => [
-            'typo3' => '6.2.20-9.0.99',
-            'php' => '5.5.0-7.1.99',
-        ],
-        'conflicts' => [],
-        'suggests' => [],
-    ],
-];
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'Sitemap Generator',
+  'description' => 'Easy to use sitemap generator for pages and records.',
+  'category' => 'plugin',
+  'author' => 'Markus Sommer',
+  'author_email' => 'markussom@posteo.de',
+  'state' => 'stable',
+  'version' => '1.1.0',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '6.2.20-9.0.99',
+      'php' => '5.5.0-7.1.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+);
