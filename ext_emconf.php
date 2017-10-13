c745d23f493adef82d165f9e9433f99cebadca8c
Markus Sommer <markussom@posteo.de>
2017-10-13 12:03:01 +0200

    [TASK] Make ext_emconf and composer.json consistent

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,8 +11,8 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' =>
     array (
-      'typo3' => '6.2.20-8.7.99',
-      'php' => '5.5.0-7.1.99',
+      'typo3' => '7.6.0-8.7.99',
+      'php' => '5.6.0-7.1.99',
     ),
     'conflicts' =>
     array (
